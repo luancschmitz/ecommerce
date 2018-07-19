290b4c5bc232ab2f1ebcae34f9824983e77e5cd2
luancschmitz <luan_c_schmitz@outlook.com>
2018-07-18 23:27:26 -0300

    tela de login

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php 
-
+session_start();
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\Model\User;
 use \Hcode\PageAdmin;
 
 
@@ -23,8 +24,32 @@ $app->get('/', function() {
 $app->get('/admin', function() {
     $page = new PageAdmin();
 
+    User::verifyLogin();
+
     $page->setTpl("index");
 
+});
+
+$app->get("/admin/login", function () {
+    $page = new PageAdmin([
+        "header" => false,
+        "footer" => false
+    ]);
+
+    $page->setTpl("login");
+});
+
+$app->post("/admin/login", function () {
+    User::login($_POST['login'], $_POST['password']);
+
+    header("location: /admin");
+});
+
+$app->get("/admin/logout", function () {
+   User::logout();
+
+   header("location: /admin/login");
+   exit;
 });
 
 $app->run();
