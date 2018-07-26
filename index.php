39ad3b8c81fb640172eac9c1f1d1286e8564dc40
luancschmitz <luan_c_schmitz@outlook.com>
2018-07-25 22:53:26 -0300

    site categorias

--- index.php
+++ index.php
@@ -275,6 +275,19 @@ $app->post("/admin/categories/:idcategory", function ($idcategory) {
     exit;
 });
 
+$app->get("/categories/:idcategory", function ($idcategory) {
+   $category = new Category();
+
+   $category->get((int)$idcategory);
+
+   $page = new Page();
+
+   $page->setTpl("category", array(
+       "category" => $category->getValues(),
+       "products" => array()
+   ));
+});
+
 $app->run();
 
  ?>
