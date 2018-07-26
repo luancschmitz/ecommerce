eec71128762a691b239a56c6bfe9733ca8b8bd56
luancschmitz <luan_c_schmitz@outlook.com>
2018-07-25 22:19:43 -0300

    admin cadastro categorias

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ require_once("vendor/autoload.php");
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\Model\User;
+use \Hcode\Model\Category;
 use \Hcode\PageAdmin;
 
 
@@ -195,6 +196,83 @@ $app->post("/admin/forgot/reset", function () {
 
     $page->setTpl("forgot-reset-success");
 
+});
+
+$app->get("/admin/categories", function () {
+    User::verifyLogin();
+
+    $page = new PageAdmin();
+
+    $categories = Category::listAll();
+
+    $page->setTpl("categories", array(
+        "categories" => $categories
+    ));
+});
+
+$app->get("/admin/categories/create", function () {
+    User::verifyLogin();
+
+    $page = new PageAdmin();
+
+    $page->setTpl("categories-create");
+});
+
+$app->post("/admin/categories/create", function () {
+    User::verifyLogin();
+
+    $category = new Category();
+
+    $category->setData($_POST);
+
+    $category->save();
+
+    header("location: /admin/categories");
+    exit;
+});
+
+$app->get("/admin/categories/:idcategory/delete", function ($idcategory) {
+    User::verifyLogin();
+
+    $category = new Category();
+
+    $category->get((int)$idcategory);
+
+    $category->delete();
+
+    header("location: /admin/categories");
+    exit;
+});
+
+$app->get("/admin/categories/:idcategory/", function ($idcategory) {
+    User::verifyLogin();
+
+    $category = new Category();
+
+    $category->get((int)$idcategory);
+
+    $page = new PageAdmin();
+
+    $page->setTpl("categories-update", array(
+        "category" => $category->getValues()
+    ));
+
+    exit;
+});
+
+$app->post("/admin/categories/:idcategory", function ($idcategory) {
+    User::verifyLogin();
+
+    $category = new Category();
+
+    $category->get((int)$idcategory);
+
+    $category->setData($_POST);
+
+    $category->save();
+
+    header("location: /admin/categories");
+    exit;
 });
 
 $app->run();
